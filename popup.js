f29f954371d61a3270c76795f96d9433a0c942cd
jackson <jackson@jacksons-MacBook-Pro.local>
2018-10-10 10:22:37 -0300

    [jackson/luciano] change number of display lines and create a input text to customize number of lines the people want

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 document.getElementById('transcription').onclick = sendMessageTab;
 document.getElementById('history').onclick = sendHistoryMessageTab;
 document.getElementById('stop').onclick = sendStopMessageTab;
+var lines = document.getElementById('lines-display-input-field')
 
 function sendMessageTab() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "transcription"}, {});
+    var displayLines = lines.value
+
+    chrome.tabs.sendMessage(tabs[0].id, {action: "transcription", lines: displayLines}, {});
   });
 }
 
