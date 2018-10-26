310549f52137b42ac725468b4bc42ceaa247dceb
jackson <jackson@jacksons-MacBook-Pro.local>
2018-10-25 21:39:22 -0300

    fix bug status button

--- popup.js
+++ popup.js
@@ -9,40 +9,25 @@ document.getElementById('history').onclick = sendHistoryMessageTab;
 
 function sendMessageTab() {
 
-  var iclass, text;
   if (startTranscription == false) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: "stop"}, {});
     });
-    
-    actionButton.textContent = ""
-    actionButton.className = "start"
-    
-    iclass = 'fa fa-closed-captioning'
-
-    text = document.createTextNode("Transcription"); 
   
   } else {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: "transcription"}, {});
     });
-
-    actionButton.textContent = ""
-    actionButton.className = "stop"
-    
-    iclass = 'fa fa-stop';
-    
-    text = document.createTextNode("Stop"); 
-      
   }
-
-  var i = document.createElement('I');
-  i.className = iclass;
-  actionButton.appendChild(i);
-  actionButton.appendChild(text);
 
 
   startTranscription = !startTranscription
+  setTranscriptionButton()
+  saveClosedButtonStatus(startTranscription)
+  
+  if (!startTranscription) {
+    window.close();
+  }
   
 }
 
@@ -61,11 +46,65 @@ function sendStopMessageTab() {
 function changeLanguage() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {"language": language.value}, {});
+    saveLanguageStatus(language)
   });
+}
+
+function saveLanguageStatus(language) {
+  chrome.storage.local.set({ "language": language.value }, function(){});
+}
+
+function saveClosedButtonStatus(status) {
+  chrome.storage.local.set({"status": status}, function(){});
+}
+
+function getButtonStatus() {
+  chrome.storage.local.get(["language"], function(languageName){
+      language.value = languageName.language
+  });
+}
+
+function loadButtonStatus() {
+  chrome.storage.local.get(["status"], function(status) {
+    var tempStatus = false
+
+    if (status) {
+      tempStatus = status.status
+    }
+    startTranscription = tempStatus
+    setTranscriptionButton()
+});
+
+}
+
+function setTranscriptionButton() {
+  var i,text,iclass
+
+  if (!startTranscription) {
+    actionButton.textContent = ""
+    actionButton.className = "stop"
+    
+    iclass = 'fa fa-stop';
+    i = document.createElement('I');
+    text = document.createTextNode("Stop"); 
+    i.className = iclass;
+  } else {
+    actionButton.textContent = ""
+    actionButton.className = "start"
+    
+    iclass = 'fa fa-closed-captioning'
+    i = document.createElement('I');
+    text = document.createTextNode("Transcription"); 
+    i.className = iclass;
+  }
+
+  actionButton.appendChild(i);
+  actionButton.appendChild(text);
 }
 
 
 window.addEventListener("DOMContentLoaded", function() {
   language = document.getElementById('teste')
-  language.addEventListener("change", changeLanguage, false);
+  language.addEventListener("change", changeLanguage, false)
+  loadButtonStatus()
 }, false);
