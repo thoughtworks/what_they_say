6bdf91b0452f80f9bc4eda4c161c4ed4e469889a
Jackson Smith <jackson.96@gmail.com>
2018-11-01 02:35:43 -0300

    add test delete words

--- src/js/helper/contentHelper.js
+++ src/js/helper/contentHelper.js
@@ -9,3 +9,19 @@ function getCurrentLanguague(language) {
     view.classList.add(className)
     return div
   }
+
+  function wordCount(str) { 
+    return str.split(" ").length;
+  }
+
+  function removeWordAtPosition(str,position) {
+    position -= 1
+    var isFirstWord = position === 0
+    var finalString = str.replace(str.substring(getPosition(str," ", position), getPosition(str," ", position + 1)), "")
+    finalString = isFirstWord ? finalString.slice(1, finalString.length) : finalString
+    return finalString
+  }
+
+  function getPosition(string, subString, index) {
+    return string.split(subString, index).join(subString).length;
+ }
--- test/specs/contentHelper.js
+++ test/specs/contentHelper.js
@@ -29,5 +29,57 @@ describe('Content Functions tests', () => {
 
 				expect(transcriptionClass) 
 					.toEqual(fixture.className); 
-				});
+			});
+
+			it('should count words quantity', () => {
+				var text = "Jamais estaremos satisfeitos "
+				 + "enquanto nossos corpos, pesados da fadiga "
+				 + "de viagem, não puderem hospedar-se nos hotéis "
+				 + "de beira de estrada e nos hotéis das cidades. "
+				 + "Não estaremos satisfeitos enquanto a mobilidade "
+				 + "básica do negro for apenas de um gueto menor para um maior. "
+				 + "Jamais estaremos satisfeitos enquanto nossas crianças tiverem "
+				 + "suas individualidades e dignidades roubadas por "
+				 + "cartazes que dizem Exclusivo para brancos."
+
+				var count = wordCount(text)
+
+				expect(62) 
+					.toEqual(count); 
+			});
+
+			it('should remove first words', () => {
+				var firstText = "Jamais estaremos satisfeitos "
+
+				var secondText = "estaremos satisfeitos "
+
+				var modifiedText = removeWordAtPosition(firstText,1)
+
+				expect(secondText) 
+					.toEqual(modifiedText); 
+			});
+
+			it('should remove second words', () => {
+				var firstText = "Jamais estaremos satisfeitos "
+
+				 var secondText = "Jamais satisfeitos "
+
+				var modifiedText = removeWordAtPosition(firstText,2)
+
+				expect(secondText) 
+					.toEqual(modifiedText); 
+			});
+
+			it('should remove third word', () => {
+				var firstText = "Jamais estaremos satisfeitos "
+				 + "enquanto nossos corpos, pesados da fadiga "
+
+				 var secondText = "Jamais estaremos "
+				 + "enquanto nossos corpos, pesados da fadiga "
+
+				var modifiedText = removeWordAtPosition(firstText,3)
+
+				expect(secondText) 
+					.toEqual(modifiedText); 
+			});
 });
