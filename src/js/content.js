92562762da71b3c0a901ffa5f98411153be8d759
Jackson Smith <jackson.96@gmail.com>
2018-11-16 10:49:36 -0200

    group messages

--- src/js/content.js
+++ src/js/content.js
@@ -97,7 +97,7 @@ recognition.onresult = function(event) {
 
   for (var i = event.resultIndex; i < event.results.length; ++i) {
     if (event.results[i].isFinal) {
-      // final_transcript += event.results[i][0].transcript;
+      final_transcript += event.results[i][0].transcript;
     } else {
       interim_transcript += event.results[i][0].transcript;
     }
